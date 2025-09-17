d102c986abd868e5857d2c78b295edb6fc86f96e
alexventura <468140.joan23@fje.edu>
2025-09-17 17:48:53 +0200

    Alex

--- login.php
+++ login.php
@@ -8,17 +8,22 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Login</title>
 </head>
 <body>
-    <label for="username">Username:</label>
-    <br>
-    <input type="text" placeholder="Username" />
-    
-    <br>
+    <form action="./dashboard.php">
+        <div class="login-container">
+            <label for="username" class="username-label">Username:</label>
+            <br>
+            <input type="text" placeholder="Enter your username" class="username-input">
+            
+            <br>
 
-    <label for="password">Password:</label>
-    <br>
-    <input type="password" placeholder="Password" />
+            <label for="password" class="username-label">Password:</label>
+            <br>
+            <input type="password" placeholder="Enter your password" class="password-input">
+        </div>
+    </form>
 </body>
 </html>

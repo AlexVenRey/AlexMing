c0254d269857bf70106092cabf5492aba31c2f1e
alexventura <468140.joan23@fje.edu>
2025-09-17 17:22:34 +0200

    Login

--- login.php
+++ login.php
@@ -1,1 +1,24 @@
-<!-- LOGIN PAGE -->
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <label for="username">Username:</label>
+    <br>
+    <input type="text" placeholder="Username" />
+    
+    <br>
+
+    <label for="password">Password:</label>
+    <br>
+    <input type="password" placeholder="Password" />
+</body>
+</html>

ebd805604f93f852797462baf6577991ca59e2f9
Myngatte <zhouming2004@gmail.com>
2025-09-17 17:50:51 +0200

    Ming2

--- database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE darkweb;
-USE darkweb;
-
-CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_user VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL 
-);
-

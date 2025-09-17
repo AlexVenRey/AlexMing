89d832d30a8c5c3c9202c57d23a5550c7702e522
Myngatte <zhouming2004@gmail.com>
2025-09-17 17:49:23 +0200

    Ming1

---
+++ Dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <!-- Chart.js -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <!-- CSS -->
+     <link rel="stylesheet" href="./CSS/Dashboard.css">
+
+</head>
+<body>
+    
+</body>
+</html>
